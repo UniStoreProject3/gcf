afd789d21707143e8c7ac1f0b36f54e451e6f8ea
Ibrohim Mubarok <befousmain@gmail.com>
2023-10-27 16:55:20 +0700

    Create MembuatTokenUser.go

---
+++ MembuatTokenUser.go
@@ -0,0 +1,29 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/Befous/BackendGIS"
+)
+
+func init() {
+	functions.HTTP("HelloHTTP", HelloHTTP)
+}
+
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.MembuatTokenUser("privatekey", "mongoenv", "befous", "user", r))
+
+}
